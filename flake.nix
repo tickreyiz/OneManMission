2319d02fdca7bb930925c8c47d3bfb219ae588dc
tickreyiz <ysfemreygt@proton.me>
2025-10-04 09:51:35 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Python geliştirme ortamı";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python312
+            python312Packages.pip
+          ];
+
+          shellHook = ''
+            echo "Ready!"
+          '';
+        };
+      });
+}
